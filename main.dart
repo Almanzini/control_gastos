e796ea6743f5f6a7a6dbe26655db694ec1cf1cd4
Almanzini <33759706+Almanzini@users.noreply.github.com>
2020-06-25 19:04:52 +0000

    Add files via upload

--- main.dart
+++ main.dart
@@ -1,9 +1,8 @@
+import 'package:control_gastos/graph_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
-void main() =>
-  runApp(MyApp());
-
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
@@ -13,7 +12,7 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
-        ),
+      ),
       home: MyHomePage(),
     );
   }
@@ -25,36 +24,122 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-
-  Widget _bottomAction (IconData icon){
-     return InkWell(
-       child: Padding(
-         padding: const EdgeInsets.all(8.0),
-         child: Icon(icon),
-       ),
-       onTap: (){},
-
-     );
+  Widget _bottomAction(IconData icon) {
+    return InkWell(
+      child: Padding(
+        padding: const EdgeInsets.all(10.0),
+        child: Icon(icon),
+      ),
+      onTap: () {},
+    );
   }
 
   @override
   Widget build(BuildContext context) {
-       return Scaffold(
-         bottomNavigationBar: BottomAppBar(
-           child: Row(
-             mainAxisSize: MainAxisSize.max,
-             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-             children: <Widget>[
-             _bottomAction(FontAwesomeIcons.history),
-             _bottomAction(FontAwesomeIcons.chartPie),
-             _bottomAction(FontAwesomeIcons.wallet),
-             _bottomAction(Icons.settings),
-           ],)
-         ),
-         floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
-         floatingActionButton: FloatingActionButton(
-           child: Icon(Icons.add),
-           onPressed: (){}),
-        );
+    return Scaffold(
+      bottomNavigationBar: BottomAppBar(
+          notchMargin: 8.0,
+          shape: CircularNotchedRectangle(),
+          child: Row(
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              _bottomAction(FontAwesomeIcons.history),
+              _bottomAction(FontAwesomeIcons.chartPie),
+              SizedBox(width: 48.0),
+              _bottomAction(FontAwesomeIcons.wallet),
+              _bottomAction(Icons.settings),
+            ],
+          )),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      floatingActionButton:
+          FloatingActionButton(child: Icon(Icons.add), onPressed: () {}),
+      body: _body(),
+    );
+  }
+
+  Widget _body() {
+    return SafeArea(
+      child: Column(
+        children: <Widget>[
+          _selector(),
+          _expenses(),
+          _graph(),
+          _list(),
+        ],
+      ),
+    );
+  }
+
+  Widget _selector() => Container();
+
+  Widget _expenses() {
+    return Column(
+      children: <Widget>[
+        Text(
+          "\$7878",
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: 40.0,
+          ),
+        ),
+        Text("Gasto Total",
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 16.0,
+              color: Colors.blueGrey,
+            )),
+      ],
+    );
+  }
+
+  Widget _graph() {
+    return Container(height: 250.0, child: GraphWidget());
+  }
+
+  Widget _item(IconData icon, String nombre, int percent, double value) {
+    return ListTile(
+      leading: Icon(
+        icon,
+        size: 32.0,
+      ),
+      title: Text(
+        nombre,
+        style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20.0),
+      ),
+      subtitle: Text(
+        "$percent% de gastos",
+        style: TextStyle(color: Colors.blueGrey),
+      ),
+      trailing: Container(
+        decoration: BoxDecoration(
+            color: Colors.blueAccent.withOpacity(0.2),
+            borderRadius: BorderRadius.circular(5.0)),
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Text("\$$value",
+              style: TextStyle(
+                  color: Colors.blueAccent,
+                  fontWeight: FontWeight.w500,
+                  fontSize: 16.0)),
+        ),
+      ),
+    );
+  }
+
+  Widget _list() {
+    return Expanded(
+        child: ListView.separated(
+            itemCount: 15,
+            itemBuilder: (BuildContext context, int index) => _item(
+                FontAwesomeIcons.shoppingCart, "Gasto en comida", 14, 145.12),
+            separatorBuilder: (BuildContext context, int index) {
+              return Container(
+                color: Colors.blueAccent.withOpacity(0.15),
+                height: 8.0,
+           );
+         }
+      )
+    );
   }
 }
